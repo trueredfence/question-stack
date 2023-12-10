c1e31890f9c665f1763eb0887271fb2acd6154fd
devtechfusion <devtechfusion@gmail.com>
2023-12-10 22:21:19 +0530

    Updated mobile menu and global search theme

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Theme from "./Theme";
-// import MobileNav from './MobileNav'
-// import GlobalSearch from '../search/GlobalSearch'
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
@@ -22,7 +22,7 @@ const Navbar = () => {
         </p>
       </Link>
 
-      {/* <GlobalSearch /> */}
+      <GlobalSearch />
 
       <div className="flex-between gap-5">
         <Theme />
@@ -41,7 +41,7 @@ const Navbar = () => {
           />
         </SignedIn>
 
-        {/* <MobileNav /> */}
+        <MobileNav />
       </div>
     </nav>
   );
